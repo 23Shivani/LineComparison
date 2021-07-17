f86f39e75da58d3ded62bb1b64bb61a490a23cef
23Shivani <shivani.gharve.2310@gmail.com>
2021-07-17 17:53:15 +0530

    Added: OOPS Refactored Program

--- src/com/bridgelabz/LineComparison.java
+++ src/com/bridgelabz/LineComparison.java
@@ -2,58 +2,58 @@ package com.bridgelabz;
 
 public class LineComparison {
 
-		// TODO Auto-generated method stub
-		public static int x1;
-		public static int y1;
-		public static int x2;
-		public static int y2;
-		public static int lengthXY,lengthAB;
-		
-		public LineComparison(int x1,int y1, int x2, int y2) {
-			this.x1=x1;
-			this.x2=x2;
-			this.y1=y1;
-			this.y2=y2;
+	public static int x1;
+	public static int y1;
+	public static int x2;
+	public static int y2;
+	public static int lengthXY,lengthAB;
+	
+	public LineComparison(int x1,int y1, int x2, int y2) {
+		LineComparison.x1=x1;
+		LineComparison.x2=x2;
+		LineComparison.y1=y1;
+		LineComparison.y2=y2;
+	}
+	
+	public static int LengthOfLine() {
+		int length;
+		int x= (int)Math.pow(x2-x1, 2);
+		int y = (int) Math.pow(y2-y1, 2);
+		length = (int)Math.sqrt(x+y);
+		return length;
+	}
+	public static Boolean lineEquality(Integer lineOne, Integer lineTwo) {
+		if (lineOne.equals(lineTwo)) {
+			return true;
 		}
-		
-		public static int LengthOfLine() {
-			int length;
-			int x= (int)Math.pow(x2-x1, 2);
-			int y = (int) Math.pow(y2-y1, 2);
-			length = (int)Math.sqrt(x+y);
-			return length;
+		return false;
+	}
+	public static void lineCompare(Integer lineOne, Integer lineTwo) {
+		int result = Integer.compare(lineOne, lineTwo);
+		if (result>0) {
+			System.out.println("LineXY is greater");
 		}
-		public static Boolean lineEquality(Integer lineOne, Integer lineTwo) {
-			if (lineOne.equals(lineTwo)) {
-				return true;
-			}
-				return false;
+		else if (result<0) {
+			System.out.println("LineAB is greater");
 		}
-		public static void lineCompare(Integer lineOne, Integer lineTwo) {
-			int result = Integer.compare(lineOne, lineTwo);
-			if (result>0) {
-				System.out.println("LineXY is greater");
-			}
-			else if (result<0) {
-				System.out.println("LineAB is greater");
-			}
-			else System.out.println("Both lines are equals");
+		else System.out.println("Both lines are equals");
+	}
+	
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Computation Program");
+		LineComparison lineXY = new LineComparison(2, 2, 5, 4);
+		Integer lengthXY = LengthOfLine();
+		LineComparison lineAB = new LineComparison(9, 4, 2, 6);
+		Integer lengthAB = LengthOfLine();
+		System.out.println(lengthAB);
+		System.out.println(lengthXY);
+		if (lineEquality(lengthXY, lengthAB)) {
+			System.out.println("Both lines are euqal with length: "+ lengthAB);
 		}
-		
-		public static void main(String[] args) {
-			System.out.println("Welcome to Line Computation Program");
-			LineComparison lineXY = new LineComparison(2, 2, 5, 4);
-			Integer lengthXY = LengthOfLine();
-			LineComparison lineAB = new LineComparison(9, 4, 2, 6);
-			Integer lengthAB = LengthOfLine();
-	//		System.out.println(lengthAB);
-	//		System.out.println(lengthXY);
-			if (lineEquality(lengthXY, lengthAB)) {
-				System.out.println("Both lines are euqal with length: "+ lengthAB);
-			}
-			else {
-				System.out.println("Both lines having different lengths");
-			}
-			lineCompare(lengthAB, lengthXY);
+		else {
+			System.out.println("Both lines having different lengths");
+		}
+		lineCompare(lengthAB, lengthXY);
 	}
 }
